3431ce7721465bb59b21a6050b537c71af96853c
EggsellentGuy <ivanrossi112@gmail.com>
2025-11-21 13:16:41 +0300

    add second step

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,17 +1,14 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 from dotenv import load_dotenv
-
 
 load_dotenv()
 
 app = Flask(__name__)
-
-
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "dev-secret")
 
 
 @app.route("/")
 def index():
-    return "Hello, Hexlet!"
+    return render_template("index.html")
